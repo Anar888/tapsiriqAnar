0119f702310a8265fdbb7c3dddaf6d964eb1a527
AnarMasiyev777 <72488405+AnarMasiyev777@users.noreply.github.com>
2021-08-18 11:30:52 +0400

    taskk

---
+++ variable/variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace variable
+{
+    class program
+
+    {
+        static void Main(string[] args)
+        {
+            string[] word = { "Baku", "Seki", "quba" };
+            Console.WriteLine(Method1(word));
+        }
+        static StringBuilder Method1(string[] word)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < word.Length; i++)
+            {
+                word[i] += ",";
+                result.Append(word[i]);
+            }
+            return result;
+        }
+    }
+}
